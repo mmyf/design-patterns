46d78f57a83ee65f0b148c4980dc15f87d55789b
fattoumi <maher.fattoumi@gmail.com>
2018-08-08 23:58:52 +0200

    Ajout des nouveaux sous projets

---
+++ composite/src/main/java/org/composite/App.java
@@ -0,0 +1,13 @@
+package org.composite;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ data-access-object/src/main/java/org/data/access/object/App.java
@@ -0,0 +1,13 @@
+package org.data.access.object;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
