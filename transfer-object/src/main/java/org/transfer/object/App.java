c80f232fb4871869e76bf40def7c32b322acf3b0
fattoumi <maher.fattoumi@gmail.com>
2018-08-08 23:50:42 +0200

    transfer-object

---
+++ transfer-object/src/main/java/org/transfer/object/App.java
@@ -0,0 +1,13 @@
+package org.transfer.object;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
